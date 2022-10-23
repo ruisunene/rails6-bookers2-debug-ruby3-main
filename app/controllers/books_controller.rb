5f7f38136f71f485057faa0e5292e8d6803b454e
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-10-23 09:28:02 +0000

    応用課題３

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ before_action :correct_user, only: [:edit, :update]
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = current_user
+    @book_comment = BookComment.new
   end
 
   def index
