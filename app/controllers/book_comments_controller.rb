a6a96ce15828a1b879d8cfdfcb44b5f8108b73b1
EC2 Default User <ec2-user@ip-172-31-46-78.ap-northeast-1.compute.internal>
2022-11-01 05:02:43 +0000

    応用課題６提出用

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,22 +3,29 @@ class BookCommentsController < ApplicationController
 
   def create
 
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer #元のURLに遷移するコード
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      #flash.now[:notice] = 'コメントを投稿しました'
+      render :book_comments
+    else
+      render 'books/show'
+    #redirect_to request.referer #元のURLに遷移するコード
     #else
       #@error_comment = comment
       #@book = Book.find(params[:book_id])
       #@book_comment = Book.new
       #render :'books/show'
     #end
+    end
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy#find(params[:id]).destroy
-    redirect_to  request.referer
+    #redirect_to  request.referer
+    @book = Book.find(params[:book_id])
+    render :book_comments
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,6 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require jquery
+//= require rails-ujs
